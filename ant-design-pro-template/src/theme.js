5ee7478c4f8e1daea587c7e0eeaa4586f00ecb46
jingren <598442441@qq.com>
2018-01-16 23:08:55 +0800

    init theme

---
+++ ant-design-pro-template/src/theme.js
@@ -0,0 +1,5 @@
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  'primary-color': '#f60',
+  // 'card-actions-background': '#f60',
+};
