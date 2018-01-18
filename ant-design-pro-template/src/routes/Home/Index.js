9a199de92fca55d054637518537b36a0e65cc9b1
geewonii <598442441@qq.com>
2018-01-18 09:14:39 +0800

    change style

--- ant-design-pro-template/src/routes/Home/Index.js
+++ ant-design-pro-template/src/routes/Home/Index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Carousel } from 'antd';
 
-import styles from './index.less';
+// import styles from './index.less';
 
 @connect(({ chart, loading }) => ({
   chart,
